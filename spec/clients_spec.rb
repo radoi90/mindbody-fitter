415b64a07659a7ff9863cc75aaa1ad04771e8785
Emil <eculic17@gmail.com>
2015-01-22 21:35:11 +0000

    update tests for new /clients/:client_is/* routes

--- spec/clients_spec.rb
+++ spec/clients_spec.rb
@@ -16,25 +16,25 @@ describe 'Mindbody Wrapper Clients' do
   end
 
   it 'should return json to client services' do
-    get '/clients/services'
+    get '/clients/1/services'
     last_response.headers['Content-Type'].must_equal \
     'application/json;charset=utf-8'
   end
 
   it 'should return json to client visits' do
-    get '/clients/visits'
+    get '/clients/1/visits'
     last_response.headers['Content-Type'].must_equal \
     'application/json;charset=utf-8'
   end
 
   it 'should return json to client purchases' do
-    get '/clients/purchases'
+    get '/clients/1/purchases'
     last_response.headers['Content-Type'].must_equal \
     'application/json;charset=utf-8'
   end
 
   it 'should return json to clients schedule' do
-    get '/clients/schedule'
+    get '/clients/1/schedule'
     last_response.headers['Content-Type'].must_equal \
     'application/json;charset=utf-8'
   end
